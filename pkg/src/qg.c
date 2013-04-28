e3dd430f8fc0de0addf2cfd462991869aa255e47
Mark van der Loo <mark.vanderloo@gmail.com>
2013-04-28 16:49:49 +0200

    Added workspace type for automatic workspace resizing

--- pkg/src/qg.c
+++ pkg/src/qg.c
@@ -1,54 +1,128 @@
+/* This is a very simple implementation of the q-gram distance.
+   q-grams for strings s and t are pushed onto an unsorted list where they
+   are counted as well. 
+*/
 
-//#include<stdlib.h>
 #include <string.h>
 #include <R.h>
 #include <Rdefines.h>
 
-//#include<stdio.h>
+
+/* Some workspace stuff */
+typedef struct{
+    unsigned int *A;
+    unsigned int nrec;
+    unsigned int recsize;
+} workspace;
+
+static workspace *new_workspace(unsigned int nrec, unsigned int recsize){
+  workspace *ws   = (workspace *) malloc(sizeof(workspace));
+  if (ws == NULL){ 
+      return NULL;
+  }
+  ws->A = (unsigned int *) calloc(sizeof(int),nrec*recsize);
+  if (ws->A == NULL){ 
+      return NULL;
+  }
+  ws->nrec            = nrec;
+  ws->recsize         = recsize;
+  return ws;   
+}
+
+static void free_workspace(workspace *ws){
+  free(ws->A);
+  free(ws);
+}
+
+static int double_workspace(workspace *ws){
+  unsigned int *A = (unsigned int *) calloc(sizeof(int), 2 * ws->nrec * ws->recsize );
+  if (A == NULL){
+    return(0);
+  }
+  unsigned int *a = ws->A;
+  ws->A = A;
+  memcpy(ws->A, a, sizeof(int) * ws->nrec * ws->recsize);
+  free(a);
+  ws->nrec    *= 2;
+  return 1;
+}
+
+static void print_workspace(workspace *ws){
+  for ( int i=0; i<ws->nrec; ++i){
+    for (int j=0; j<ws->recsize; ++j ){
+      Rprintf("%d, ",ws->A[i*ws->recsize + j]);
+    }
+    Rprintf("\n");
+  }
+}
+
+static void reset_workspace(workspace *ws){
+  memset(ws->A, 0, sizeof(int) * ws->nrec * ws->recsize);
+}
+
+/* Actual work starts here */
 
 static int qgram_equals(unsigned int *x, unsigned int *y, int q){
-  int i=0;
+  int i = 0;
   while ( i < q & x[i] == y[i] ) ++i;
   return ( i == q ) ? 1 : 0;
 }
 
-static int add_qgram(unsigned int *s, int q, int offset, unsigned int *work, unsigned int nfilled){
+
+static int add_qgram(
+        unsigned int *s, 
+        int q, 
+        int offset, 
+        workspace *work,
+        unsigned int nfilled
+){
   size_t qg_size = sizeof(unsigned int) * q;
   int i = 0; 
   int i_pos = 0;
+
   // Find out if qgram already exists in workspace
-  while ( i < nfilled & !qgram_equals(s, work + i_pos, q) ){
+  while ( i < nfilled & !qgram_equals(s, work->A + i_pos, q) ){
       ++i;
       i_pos += q + 2;
   }
+
   // if not, add qgram
   if ( i == nfilled ){
-    memcpy(work + i_pos, s, qg_size);
+    memcpy(work->A + i_pos, s, qg_size);
     nfilled += 1;
   }
+
   // increase counter for qgram.
-  work[i_pos + q + offset] += 1;
+  work->A[i_pos + q + offset] += 1;
+
+  // double workspace size if workspace is full
+  if (nfilled == work->nrec){
+    if ( !double_workspace(work) ){
+      free_workspace(work);
+      error("Could not allocate memory");
+    }
+  }
+
   // return number of stored qgrams in workspace.
   return nfilled;
 }
 
 
 // Compute qgram distance using L1-distance between sparse vectors
-// This is the naive implementation, but its fast when comparing short strings.
+// This is a naive implementation, but its fast when comparing short strings.
 int qgram(
       unsigned int *s, 
       unsigned int *t, 
       unsigned int x, 
       unsigned int y, 
       unsigned int q, 
-      unsigned int *work
+      workspace *work
     ){
 
   // return -1 when q is larger than one of the strings.
   if ( q > (x >= y ? x : y) ) return -1;
   int nfill = 0;
   // store and count qgrams.
-  // TODO: add workspace doubler.
   for (int i=0; i < x-q+1; ++i){
     nfill = add_qgram(s+i, q, 0, work, nfill);
   }
@@ -58,8 +132,9 @@ int qgram(
   int L1 = 0;
   int ncols = q + 2;
   for ( int j=0; j < nfill; j++ ){
-    L1 = L1 + abs(work[j*ncols + q] - work[j*ncols + q + 1]);
+    L1 +=  abs(work->A[j*ncols + q] - work->A[j*ncols + q + 1]);
   }
+  reset_workspace(work);
   return L1;
 }
 
@@ -82,8 +157,11 @@ SEXP R_qgram(SEXP a, SEXP b, SEXP qq){
   PROTECT(yy = allocVector(REALSXP, nt));
   double *y = REAL(yy);
 
-  // ask for 1M of ints
-  unsigned int *work = (unsigned int *) calloc(sizeof(unsigned int),1024*1024);
+  // Enough room to store 10k unique q-grams and counters (will be increased when necessary).
+  workspace *work = new_workspace(10000,q+2);
+  if (work == NULL){
+    error("Could not allocate enough memory");
+  }
   for ( k=0; k < nt; ++k ){
     i = k % na;
     j = k % nb;
@@ -105,30 +183,3 @@ SEXP R_qgram(SEXP a, SEXP b, SEXP qq){
   return yy;
 }
 
-/*
-void main(){
-  unsigned int s[4] = {0,1,2,3};
-  unsigned int t[5] = {2,3,4,5,6};
-  unsigned int *w = malloc(sizeof(int)*1000);
-
-
-  int d = qg_dist_def(s,t,4,5,2,w,10);
-  printf("%d\n",qgram_equals(w,w,1));
-
-  printf("%d\n",d); 
-  free(w);
-
-
-}
-
-static void print_work(unsigned int *w,int q, int nrow){
-  for ( int j=0; j<nrow; ++j){
-    for( int i=0; i < q+2; ++i ){
-      printf("%d ",w[j*(q+2) + i]);
-    }
-    printf("\n");
-  }
-
-}
-
-*/
