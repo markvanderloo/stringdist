d67709278a23bb0160b3e1b197b8bd51ea73a5dc
mark <mark.vanderloo@gmail.com>
2013-07-16 23:05:20 +0200

    doc bug in example of amatch

--- examples/amatch.R
+++ examples/amatch.R
@@ -1,14 +1,17 @@
 
 # lets see which sci-fi heroes are stringdistantly nearest
-amatch("leia",c("uhura","leela"))
+amatch("leia",c("uhura","leela"),maxDist=5)
 
 # we can restrict the search
 amatch("leia",c("uhura","leela"),maxDist=1)
 
-# this is always true (maxDist is Inf by default)
-ain("leia",c("uhura","leela"))
+# setting nomatch returns a different value when no match is found
+amatch("leia",c("uhura","leela"),maxDist=1,nomatch=0)
 
-# so we set a reasonable maxDist
-ain("leia",c("uhura","leela"), maxDist=1)
+# this is always true if maxDist is Inf
+ain("leia",c("uhura","leela"),maxDist=Inf)
+
+# Let's look in a neighbourhood of maximum 2 typo's (by default, the OSA algorithm is used)
+ain("leia",c("uhura","leela"), maxDist=2)
 
 
