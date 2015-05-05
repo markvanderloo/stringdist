8012a1ab1ef52a5c2e5130e82a531e5460896829
mark <mark.vanderloo@gmail.com>
2015-05-05 08:50:47 +0200

    new, general amatch interface

--- pkg/src/Rstringdist.c
+++ pkg/src/Rstringdist.c
@@ -72,21 +72,21 @@ SEXP R_stringdist(SEXP a, SEXP b, SEXP method, SEXP weight, SEXP p, SEXP q, SEXP
   #endif
   {
 
-  Stringdist *sd = R_open_stringdist( (Distance) INTEGER(method)[0]
-      , ml_a, ml_b
-      , weight
-      , p
-      , q
-  );
+    Stringdist *sd = R_open_stringdist( (Distance) INTEGER(method)[0]
+        , ml_a, ml_b
+        , weight
+        , p
+        , q
+    );
 
-  unsigned int *s = NULL, *t = NULL;
-  s = (unsigned int *) malloc(( 2L + ml_a + ml_b) * sizeof(int));
+    unsigned int *s = NULL, *t = NULL;
+    s = (unsigned int *) malloc(( 2L + ml_a + ml_b) * sizeof(int));
 
-  if ( (sd==NULL) | (bytes && s == NULL) ) nt = -1;
-  t = s + ml_a + 1L;
-    
-  int len_s, len_t, isna_s, isna_t
-    , i = 0, j = 0, ID = 0, num_threads = 1;
+    if ( (sd==NULL) | (bytes && s == NULL) ) nt = -1;
+    t = s + ml_a + 1L;
+      
+    int len_s, len_t, isna_s, isna_t
+      , i = 0, j = 0, ID = 0, num_threads = 1;
 
     #ifdef _OPENMP
     ID = omp_get_thread_num();
@@ -115,5 +115,104 @@ SEXP R_stringdist(SEXP a, SEXP b, SEXP method, SEXP weight, SEXP p, SEXP q, SEXP
   if (nt < 0 ) error("Unable to allocate enough memory");
   return(yy);
 }
+
+/* amatch
+ *
+ */
+SEXP R_amatch(SEXP x, SEXP table, SEXP method 
+  , SEXP nomatch, SEXP matchNA
+  , SEXP weight, SEXP p, SEXP q
+  , SEXP maxDistance, SEXP useBytes
+  , SEXP nthrd){
+
+  PROTECT(x);
+  PROTECT(table);
+  PROTECT(method);
+  PROTECT(nomatch);
+  PROTECT(matchNA);
+  PROTECT(weight);
+  PROTECT(p);
+  PROTECT(q);
+  PROTECT(maxDistance);
+  PROTECT(useBytes);
+  PROTECT(nthrd);
+
+  int nx = length(x)
+    , ntable = length(table)
+    , no_match = INTEGER(nomatch)[0]
+    , match_na = INTEGER(matchNA)[0]
+    , bytes = INTEGER(x)[0]
+    , ml_x = max_length(x)
+    , ml_t = max_length(table);
+
+
+  double maxDist = REAL(maxDistance)[0];
+
+  // convert to integer. 
+  Stringset *X = new_stringset(x, bytes);
+  Stringset *T = new_stringset(table, bytes);
+
+  // output vector
+  SEXP yy;
+  PROTECT(yy = allocVector(INTSXP, nx));
+  int *y = INTEGER(yy);
+  
+  #ifdef _OPENMP
+  int nthreads = INTEGER(nthrd)[0];
+  #pragma omp parallel num_threads(nthreads) default(none) \
+    shared(X, T, y, R_PosInf, NA_INTEGER, nx, ntable, no_match, match_na, bytes, ml_x, ml_t, method, weight, p, q, maxDist)
+  #endif
+  {
+    /* claim space for workhorse */
+
+    Stringdist *sd = R_open_stringdist( (Distance) INTEGER(method)[0]
+        , ml_x, ml_t
+        , weight
+        , p
+        , q
+    );
+    double d = R_PosInf, d1 = R_PosInf;
+    int index, len_X, len_T;
+    unsigned int *str;
+    unsigned int **tab;
+
+    #ifdef _OPENMP
+    #pragma omp for
+    #endif
+    for ( int i=0; i<nx; i++){
+      index = no_match;
+      len_X = X->str_len[i]; 
+      d1 = R_PosInf;
+      str = X->string[i];
+      tab = T->string;
+      for ( int j=0; j<ntable; j++, tab++){
+        len_T = T->str_len[j];
+        if (len_X != NA_INTEGER && len_T != NA_INTEGER ){        // both are char (usual case)
+          d = stringdist(sd, str, len_X, *tab, len_T);
+          if ( d <= maxDist && d < d1){ 
+            index = j + 1;
+            if ( d == 0.0 ) break;
+            d1 = d;
+          }
+        } else if ( len_X == NA_INTEGER && len_T == NA_INTEGER ) {  // both are NA
+          index = match_na ? j + 1 : no_match;
+          break;
+        }
+      }
+      
+      y[i] = index;
+    }
+    close_stringdist(sd);
+  } // end of parallel region
+  free_stringset(X);
+  free_stringset(T);
+  UNPROTECT(12);
+  if (nx < 0 ) error("Unable to allocate enough memory");
+  return(yy);
+} // end R_amatch
+
+
+
+
 
 
