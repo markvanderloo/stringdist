586112c30b9bf11dba93ddf012495d403a18db8a
mark <mark.vanderloo@gmail.com>
2015-06-18 10:44:33 +0200

    added tests for stringsim

--- pkg/tests/testthat/testStringsim.R
+++ pkg/tests/testthat/testStringsim.R
@@ -21,5 +21,30 @@ for (method in c("osa", "lv", "dl", "hamming", "lcs", "qgram", "cosine",
 }
 
 
+test_that("Stringsim gets correct values with or without useBytes",{
+  x <- "ao"
+  y <- paste0("a",intToUtf8(0x00F6)) # o-umlaut
+  expect_equal(stringsim(x,y,method="osa", useBytes=FALSE), 1-1/2)
+  expect_equal(stringsim(x,y,method="osa", useBytes=TRUE ), 1-2/3)
+  expect_equal(stringsim(x,y,method="lv", useBytes=FALSE),  1-1/2)
+  expect_equal(stringsim(x,y,method="lv", useBytes=TRUE ),  1-2/3)
+  expect_equal(stringsim(x,y,method="dl", useBytes=FALSE),  1-1/2)
+  expect_equal(stringsim(x,y,method="dl", useBytes=TRUE ),  1-2/3)
+  expect_equal(stringsim(x,y,method="hamming", useBytes=FALSE), 1-1/2)
+  expect_equal(stringsim(x,y,method="hamming", useBytes=TRUE ), 1-1)
+  expect_equal(stringsim(x,y,method="lcs", useBytes=FALSE),     1-1/2)
+  expect_equal(stringsim(x,y,method="lcs", useBytes=TRUE ),     1-3/5)
+  expect_equal(stringsim(x,y,method="qgram", q=1, useBytes=FALSE), 1-1/2)
+  expect_equal(stringsim(x,y,method="qgram", q=1, useBytes=TRUE ), 1-3/5)
+  expect_equal(stringsim(x,y,method="cosine", q=1, useBytes=FALSE), 1-1/2)
+  expect_equal(stringsim(x,y,method="cosine", q=1, useBytes=TRUE ), 1-(1-1/sqrt(6)))
+  expect_equal(stringsim(x,y,method="jaccard", q=1, useBytes=FALSE), 1-2/3)
+  expect_equal(stringsim(x,y,method="jaccard", q=1, useBytes=TRUE ), 1-3/4)
+  expect_equal(stringsim(x,y,method="jw", useBytes=FALSE), 1-1/3)
+  expect_equal(stringsim(x,y,method="jw", useBytes=TRUE ), (1/2 + 1/3 +1)/3)
+})  
+
+
+
 
 
